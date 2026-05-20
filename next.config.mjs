af27d86668a4f0b5d5163c1acfce3b11e486f216
Jartible <jartibledev@proton.me>
2026-05-21 00:38:45 +0200

    Added structure of files

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
+
+
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin(
+  './src/i18n/request.js' 
+);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  compiler:{
+    styledComponents: true,
+  },
+  transpilePackages: ['styled-components'],
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
