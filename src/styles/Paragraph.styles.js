b67238462d69d53c9f3f72b939bca778d972e1da
Jartible <jartibledev@proton.me>
2026-05-23 16:11:31 +0200

    Write praragraph

--- src/styles/Paragraph.styles.js
+++ src/styles/Paragraph.styles.js
@@ -1,227 +1,44 @@
 import styled from "styled-components";
- 
-const greyColor= " #686868"
+  
+const characterTypo = 'monospace';
 
-
-
-export const Head = styled.h1`
-    padding:0.5rem;
-    margin: 0.5rem;
-    font-family: 'Dm Mono' ;
-    font-size: 3rem ;
-    font-weight: normal;
-    color : black;
-    letter-sparcing: -5%;
-    line-height: 110%; 
+export const Character = styled.h3`
+    margin-bottom: ${props => props.$marginbottom || "0.9em"}; 
+    color: ${props => props.$color || "black"},
+    font-size: ${props => props.$fontsize || "1.3rem"};
+    font-family: ${characterTypo}; 
+    text-transform: uppercase;    
 `;
 
-export const HeadPhilosophy = styled.h2`
-    font-family: 'Dm Mono' ;
-    font-size: 3em ;
-    font-weight: normal;
-    color : black;
-    text-align: right;
-    letter-sparcing: -5%;
-    line-height: 110%;
-    margin-bottom: 5%;
-    
+export const TextCharacter = styled.p`
+    margin: ${props => props.$margin || "0"}; 
+    color: ${props => props.$color || "black"},
+    font-size: ${props => props.$fontsize || "1.2rem"};
+    font-family: ${characterTypo};
+    line-height: ${props => props.$lineheight || "1.6"};
+    white-space: ${props => props.$whitespace || "pre-wrap"};
 `;
 
-export const Subtitle = styled.h2`
-    font-family: 'Dm Mono';
-    font-weight: medium;
-    font-size: 0.9 rem ;
-    color : ${greyColor} ;
-
+export const TextNarrator = styled.p`
+    font-style: ${props => props.$fontstyle || "normal"}; 
+    font-variant: ${props => props.$fontvariant || "normal"}; 
+    font-size: ${props => props.$fontsize || "1.5rem"};
+    font-family: ${characterTypo};
+    line-height: ${props => props.$lineheight || "1.6"};
+    letter-spacing: ${props => props.$lettersparcing || "10px"};;
 `;
 
-export const Dropline = styled.h3`
-    font-family:"Geist Mono", monospace;
-    font-weight: bold;
-    font-size: 0.9em ;
-    color : ${greyColor} ;
-    text-align: right;
+.element {
+  border: 3px solid rgba(255, 255, 255, 1);
+  border-radius: 8px;
+  background-color: rgba(41, 41, 41, 0.5);
+  color: rgba(255, 255, 255, 0.86);
+  backdrop-filter: blur(9px);
+  -webkit-backdrop-filter: blur(9px);
+}
 
-`;
-
-export const Contact = styled.p`
-    font-family: 'Dm Mono';
-    font-weight: normal;
-    font-size: 1 rem ;
-    color : black ;
-     line-height: 145%; 
-
-`;
-export const Button = styled.p`
-    font-family: "Cascadia Mono", sans-serif;
-    font-weight: 600 ;
-    font-size: 1 rem ;
-    color : black ;
-    line-height: 145%; 
-
-`;
-export const Footer = styled.p`
-    font-family: 'Dm Mono';
-    font-weight: medium ;
-    font-size: 2 rem ;
-    color : black ;
-    letter-spacing: -2%;
-     line-height: 145%; 
-
-`;
-
-export const FooterText = styled.p`
-    font-family: 'Dm Mono';
-    font-weight: medium ;
-    font-size: 1 rem ;
-    color : ${greyColor} ;
-    letter-spacing: -2%;
-    line-height: 145%;
-    white-space: normal; 
-    word-wrap: break-word;
-    overflow-wrap: break-word; 
-
-`;
-export const FooterSubtitle = styled.p`
-    font-family: 'Dm Mono';
-    font-weight: medium ;
-    font-size: 1 rem ;
-    color : ${greyColor} ;
-    letter-spacing: -2%;
-     line-height: 145%; 
-
-`;
-export const Projects = styled.p`
-    font-family: "Fragment Mono", monospace;
-    font-weight: regular ;
-    font-size: 2 rem ;
-    color : black ;
-    letter-spacing: -2%;
-    line-height: 120%;
-    white-space: normal; 
-    word-wrap: break-word;
-    overflow-wrap: break-word; 
-
-`;
-
-export const DateFooter = styled.p`
-    font-family: "Fragment Mono", monospace;
-    font-weight: regular ;
-    font-size: 1.25em ;
-    
-    color : ${greyColor} ;
-    letter-spacing: -2%;
-     line-height: 120%; 
-
-`;
-export const ReachOut = styled.h3`
-    font-family: "Dm Mono", monospace;
-    font-weight: medium ;
-    font-size: 2em ;
-    color : black ;
-    letter-spacing: -2%;
-    line-height: 120%;
-    margin-bottom: 1em; 
-
-`;
-
-export const LabelSocialMedia = styled.p`
-    font-family: "Fragment Mono", monospace;
-    font-weight: regular ;
-    font-size: 2em ;
-    color : black ;
-    letter-spacing: -2%;
-    line-height: 120%;
-    margin-bottom: 1em;  
-
-`;
-
-export const SocialNetwork = styled.p`
-    font-family: "Fragment Mono", monospace;
-    font-weight: regular ;
-    font-size: 0.8em ;
-    color : black ;
-    line-height: 145%;
-    margin-bottom: 0.5em;
-     &:hover {
-            filter:blur(2px);
-            backdrop-filter: blur(2px);
-        }   
-
-`;
-export const PortafolioPress = styled.p`
-    font-family: "Cutive Mono", monospace;
-    font-weight: regular ;
-    font-size: 2.5em ;
-    color : black ;
-    letter-spacing: -2.5%;
-     line-height: 110%; 
-
-`;
-
-export const SectionPressed = styled.p`
-    font-family: 'Dm Mono';
-    font-weight: lighter ;
-    font-size: 3 rem ;
-    color : black ;
-    letter-spacing: -2.5%;
-     line-height: 110%; 
-
-`;
-
-export const Portafolio = styled.p`
-    margin: 1em;
-    font-family:"Inter", sans-serif;
-    font-weight: bold ;
-    font-size: 1.5em ;
-    color : black ;
-    letter-spacing: -2.5%;
-     line-height: 110%; 
-
-`;
-export const Section = styled.h1`
-    margin: 0px 10px 10px 0px  ;
-    font-family: 'Dm Mono';
-    font-size: 2.5em ;
-    font-weight: normal;
-    color : black;
-    letter-sparcing: -5%;
-    line-height: 110%; 
-`;
-export const Work = styled.p`
-    font-family: "Dm Mono";
-    font-weight: medium ;
-    font-size: 2em ;
-    color : black ;
-    letter-spacing: -2%;
-    line-height: 120%; 
-
-`;
-export const DateWork = styled.p`
-    font-family: "Fragment Mono", monospace;
-    font-weight: regular ;
-    font-size: 2em ;
-    
-    color : ${greyColor} ;
-    letter-spacing: -2%;
-     line-height: 120%; 
-
-`;
-export const Courses = styled.p`
-    font-family: "Dm Mono";
-    font-weight: medium ;
-    font-size: 1em ;
-    color : gray ;
-    line-height: 145%; 
-
-`;
-
-
-
-
-
-
- 
-
-
+.text {
+  font: normal normal 25px/1.2 "proxima-nova";
+  letter-spacing: 10px;
+}
 
