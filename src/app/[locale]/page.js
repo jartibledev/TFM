1b67e4c2fb8d7568268d65327803a533e3e228b6
Jartible <jartibledev@proton.me>
2026-05-23 02:01:39 +0200

    Debugging AI

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -43,7 +43,7 @@ export default function GameEngine() {
 
   let finalText = pageDates.text;
   if (pageDates.variants_text) {
-    finalText = pageDates.variants_text[lastDecition] || pageDates.text;
+    finalText = pageDates.variants_text[lastDecision] || pageDates.text;
   }
 
   const positionDefault = {
@@ -56,30 +56,30 @@ export default function GameEngine() {
   : { position: 'absolute', ...positionDefault}
 
   return (
-    <div className="contenedor-libre" style={{ backgroundImage: `url(${pageDates.fondo})` }}>
+    <div className="contenedor-libre" style={{ backgroundImage: `url(${pageDates.background})` }}>
       
       <div className="caja-dialogo"
       style={styleBox}
       onClick={(e) => e.stopPropagation()}
       >
 
-        <h3>{pageDates.personaje}</h3>
-        <p>{pageDates.text}</p> {/* Renderiza el text dinámico */}
+        <h3>{pageDates.character}</h3>
+        <p>{finalText}</p> {/* Renderiza el text dinámico */}
 
         {/* Si la página tiene opciones */}
-        {pageDates.opciones && pageDates.opciones.map((opcion, i) => (
+        {pageDates.options && pageDates.options.map((option, i) => (
           <button 
             key={i} 
-            onClick={() => advancePage(opcion.siguiente, opcion.decision)}
+            onClick={() => advancePage(option.next, option.decition)}
           >
-            {opcion.text}
+            {option.text}
           </button>
         ))}
 
         {/* Si es una página de continuación lineal */}
-        {pageDates.siguiente && (
-          <button onClick={() => advancePage(pageDates.siguiente)}>
-            Continuar...
+        {pageDates.next && !pageDates.options && (
+          <button onClick={() => advancePage(pageDates.next)}>
+            Next...
           </button>
         )}
       </div>
