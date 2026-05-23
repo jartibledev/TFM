60c57809fddd8493f6e3124a99099dced4d70771
Jartible <jartibledev@proton.me>
2026-05-23 20:13:48 +0200

    Create textPagination

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -8,6 +8,7 @@ import { Character, TextCharacter } from '@/styles/Paragraph.styles';
 
 export default function GameEngine() {
   const [actualPage, setActualPage] = useState('page_1');
+  const [subPage, setSubPage] = useState(0);
   const [initialized, setInitialized ] = useState(false);
   const [lastDecition, setLastDecition] = useState('');
   const t = useTranslations('VisualNovel.introduction');
@@ -15,6 +16,7 @@ export default function GameEngine() {
   useEffect(() => {
     const saveGame = localStorage.getItem( 'actual_page');
     const saveDecition = localStorage.getItem('last_decition');
+    const saveSubPage = localStorage.getItem('actual_sub_page');
     if (saveGame){
       setActualPage(saveGame);
     }
@@ -30,14 +32,16 @@ export default function GameEngine() {
     if (initialized) {
       localStorage.setItem('actual_page', actualPage)
       localStorage.setItem('last_decition', lastDecition);
+      localStorage.setItem('actual_sub_page', subPage.toString());
     }
-  }, [actualPage,lastDecition, initialized ]);
+  }, [actualPage,lastDecition, subPage, initialized ]);
 
   if (!initialized) {
     return <div style={{color: 'white'}}>Loading Story...</div>
   }
 
   const pageDates = t.raw(actualPage);
+
 
   const advancePage = (nextKey, decitionTaken) => {
     if (decitionTaken) {
@@ -51,6 +55,26 @@ export default function GameEngine() {
     finalText = pageDates.variants_text[lastDecition] || pageDates.text;
   }
 
+  const textParagraphs = finalText.split('\n\n').filter(p => p.trim() != "");
+  const currentTextChunk = textParagraphs[subPage] || textParagraphs[0];
+
+  const handleNextClick = () => {
+    if (subPage < textParagraphs.length -1){
+      setSubPage(prev => prev + 1 );
+    } else {
+      if (pageDates.next){
+        setActualPage(pageDates.next);
+        setSubPage(0);
+      }
+    }
+  };
+
+  const handleOptionClick = (nextKey, decitionTaken) => {
+    if (decitionTaken) setLastDecition(decitionTaken);
+    setActualPage(nextKey);
+    setSubPage(0);
+  }
+
   const positionDefault = {
     bottom: ' 40px',
     left: '50%',
@@ -60,29 +84,39 @@ export default function GameEngine() {
   ? { position: 'absolute', ...pageDates.position}
   : { position: 'absolute', ...positionDefault}
 
+  const isEndOfText = subPage === textParagraphs.length -1;
   return (
     <ArticleComponent >
       <ContainerIllustrations style={{ backgroundImage: `url(${pageDates.background})` }}>
       <MenuConfiguracion></MenuConfiguracion>
+
       <Box style={styleBox}
       onClick={(e) => e.stopPropagation()}>
       <Character>{pageDates.character}</Character>
-      <TextCharacter>{finalText}</TextCharacter>
-      {pageDates.options && pageDates.options.map((option, i) => (
-          <ButtonComponent 
-            key={i} 
-            onClick={() => advancePage(option.next, option.decition)}
-          >
-            {option.text}
-          </ButtonComponent>
-        ))}
+      <TextCharacter>{currentTextChunk}</TextCharacter>
 
+      {!isEndOfText && (
+      <ButtonComponent onClick={handleNextClick}>
+        Continue...
+      </ButtonComponent>
+        )}
 
-        {pageDates.next && !pageDates.options && (
-          <ButtonComponent onClick={() => advancePage(pageDates.next)}>
-            Next page
+        {isEndOfText && pageDates.next && !pageDates.options && (
+          <ButtonComponent onClick={handleNextClick}>
+            Continue
           </ButtonComponent>
         )}
+      
+      {isEndOfText && pageDates.options && pageDates.options.map((option, i) => (
+        <ButtonComponent 
+          key={i} 
+          onClick={() => handleOptionClick(option.next, option.decision || option.decition)}
+        >
+          {option.text}
+        </ButtonComponent>
+      ))}
+
+
       </Box>
         
       </ContainerIllustrations>
