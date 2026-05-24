9a143eb1d7bed1e6e2aa3969a19c766221d6a748
Jartible <jartibledev@proton.me>
2026-05-24 09:12:18 +0200

    Modify song_1

--- public/music/song_1.js
+++ public/music/song_1.js
@@ -1,10 +1,11 @@
 
+export const mainTheme = (strudel) => {
 const chorus ={
-  A0 : "a e c",
-  A1 : "g e b",
-  B0 : "f# d a",
-  A0P : "a c# g a"
-  }
+A0 : "a e c",
+A1 : "g e b",
+B0 : "f# d a",
+A0P : "a c# g a"
+}
 const verse = {
   C0 : "c a!2 e",
   C1 : "b e!2 a",
@@ -18,24 +19,16 @@ const refrain = {
   R0P : "a c# g a/2"
 }
 
-$: chord("<Am Em D A7>").voicing().sound("gm_acoustic_bass")._punchcard()
-$: chord("<Am Em D A7>").voicing().sound("gm_electric_guitar_muted").room(.5)._punchcard()
+strudel.chord("<Am Em D A7>").voicing().sound("gm_acoustic_bass")
+strudel.chord("<Am Em D A7>").voicing().sound("gm_electric_guitar_muted").room(.5)
 
 
-$:cat(chorus.A0, chorus.A1, chorus.B0, chorus.A0P,
+strudel.cat(chorus.A0, chorus.A1, chorus.B0, chorus.A0P,
       chorus.A0, chorus.A1, chorus.B0, chorus.A0P,
       verse.C0, verse.C1, verse.D0, verse.C0P,
      refrain.R0,refrain.R1, refrain.F0, refrain.R0P )
   .note()
   .sound("piano,vibraphone_soft")
-  ._punchcard()
-//gm_distortion_guitar
-/*$:cat(chorus.A0, chorus.A1, chorus.B0, chorus.A0P,
-      chorus.A0, chorus.A1, chorus.B0, chorus.A0P,
-      verse.C0, verse.C1, verse.D0, verse.C0P)
-  .note()
-  .sound("gm_electric_guitar_clean")
-  ._punchcard()*/
 
 
 
@@ -43,4 +36,5 @@ $:cat(chorus.A0, chorus.A1, chorus.B0, chorus.A0P,
 
 
 
-$:sound("ht <mt rd>*2 ").bank("RolandTR909")._punchcard()
+strudel.sound("ht <mt rd>*2 ").bank("RolandTR909")  
+};
