a763d309ee6f84f84ae18aacac3e9b2cf5e15063
Jartible <jartibledev@proton.me>
2026-05-24 09:20:58 +0200

    Modify MusicProvider to play complex songs

--- src/styles/MusicContext.js
+++ src/styles/MusicContext.js
@@ -1,24 +1,37 @@
 import React, { createContext, useContext, useEffect, useState  } from 'react';
+import { mainTheme } from '../../public/music/song_1';
 
 const MusicContext = createContext(null);
 
 export function MusicProvider({ children }) {
     const [strudel, setStrudel] = useState(null);
     const [isPlaying, setIsPlaying ] = useState(false);
-
+    const songs = {
+        main_theme: mainTheme, 
+    };
+    
     useEffect(() => {
         import('@strudel/webaudio').then(({ webAudioControls }) => {
             setStrudel(webAudioControls);
         });
     }, []);
 
-    const playPattern = ( codeString ) => {
+    const playPattern = ( songKey ) => {
         if (!strudel ) return;
 
         try {
             strudel.initWebAudio();
-            strudel.play(codeString);
+            strudel.stop();
+            if (songs[songKey]){
+                strudel.paly(() => {
+                    songs[songKey](strudel);
+                });
+            }else {
+                strudel.play(songKey);
+            }
+
             setIsPlaying(true);
+            
         }catch (error) {
             console.error("Error loading Strudel:", error);
         }
